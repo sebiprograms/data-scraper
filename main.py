a83fc2b644280fc4526967b84d6951e4bcd0396d
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2024-06-14 16:20:54 -0500

    Adds library dependencies and creation of while loop to run until index page is not found

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://books.toscrape.com/catalogue/page-1.html"
+
+page = requests.get(url)
+
+print(page.text)
+
+soup = BeautifulSoup(page.text,"html.parser")
+
+print(soup.title.text)

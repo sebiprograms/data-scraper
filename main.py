8f56441f1f7223f439098efbd8b5367c9c6bdc35
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2024-06-14 16:50:05 -0500

    Created item to hold book attributes

--- main.py
+++ main.py
@@ -4,19 +4,31 @@ import pandas as pd
 
 current_page = 1
 
-page = requests.get(url)
-
-print(page.text)
-
-soup = BeautifulSoup(page.text,"html.parser")
-
 proceed = True
 # collect data while proceed is true
 while(proceed):
   url = "https://books.toscrape.com/catalogue/page-"+str(current_page)+".html"
+
+  page = requests.get(url)
+
+  soup = BeautifulSoup(page.text,"html.parser")
+
   if (soup.title.text == "404 Not Found"):
     proceed = False
   else:
     all_books = soup.find_all("li",class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
-    
+
+    for book in all_books:
+      item = {}
+      item['Title'] = book.find("img").attrs["alt"]
+
+      item['Link'] = book.find("a").attrs["href"]
+
+      item['Price'] = book.find("p", class_="price_color").text[2:]
+
+      item['Stock'] = book.find("p", class_="instock availability").text.strip()
+
+      print(item['Stock'])
+
   current_page += 1
+  proceed = False
